353c7269347a9ba60fe4505f46c6863186d079d1
grinner <a.grinner@gmail.com>
2011-07-26 22:17:09 -0400

    update

--- scripts/toggler.js
+++ scripts/toggler.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
 	
 	$(".toggle_container").hide();
  
+    $("div.trigger").toggle(function(){
+		$(this).addClass("active"); 
+		}, function () {
+		$(this).removeClass("active");
+	});
+ 
 	$("div.trigger").click(function(){
 		$(this).toggleClass("active").next().slideToggle("slow");
 	});
